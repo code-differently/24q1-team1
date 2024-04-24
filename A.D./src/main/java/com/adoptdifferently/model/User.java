662fd6a51437478ef9b8e8d09a1354d313f62375
Moibrahi7 <160788202+Moibrahi7@users.noreply.github.com>
2024-04-24 16:35:41 -0400

    Feat: added Admin Controller base and setters and getters for the models (#8)
    * Feat: added base file structures
    
    * Feat: added Admin Controller base and setters and getters for the models
    
    ---------
    
    Co-authored-by: Mohamed <“moibrahi7@gmail.com”>

--- A.D./src/main/java/com/adoptdifferently/model/User.java
+++ A.D./src/main/java/com/adoptdifferently/model/User.java
@@ -1,4 +1,4 @@
-public class user {
+public class User {
     private String name;
     private String email;
     private String phoneNumber;
@@ -6,4 +6,133 @@ public class user {
     private String country;
     private String city;
     private String postcode;
+    private long userId;
+    private Set<CatListing> catListings = new HashSet<>(); // <1>
+
+    User(String name, String email, String phoneNumber, String password, String country, String city, String postcode, long userId) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.country = country;
+        this.city = city;
+        this.postcode = postcode;
+        this.userId = userId;
+    }
+    // <2>
+    /**
+     * Getters and setters
+     */
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public boolean isAdmin() {
+        return false;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Set<CatListing> getCatListings() {
+        catListings.forEach(catListing -> catListing.setOwnerId(userId));
+        Set<CatListing> list = catListings;
+        return list;
+    }
+
+    public void setCatListings(Set<CatListing> catListings) {
+        this.catListings = catListings;
+    }
+
+    // add and remove from catListings
+    public void addCatListing(CatListing catListing) {
+        catListings.add(catListing);
+    }
+
+    public void removeCatListing(CatListing catListing) {
+        catListings.remove(catListing);
+    }
+
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", email=" + email + ", phoneNumber=" + phoneNumber + ", password=" + password
+                + ", country=" + country + ", city=" + city + ", postcode=" + postcode + ", userId=" + userId
+                + ", catListings=" + catListings + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        return userId == other.userId;
+    }
+
 }
+
