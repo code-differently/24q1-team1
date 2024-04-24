3d2e76e456805f9809bde01894152ca8cae79ddd
Moibrahi7 <160788202+Moibrahi7@users.noreply.github.com>
2024-04-24 15:14:14 -0400

    Feat: added base file structures (#6)
    Co-authored-by: Mohamed <“moibrahi7@gmail.com”>

---
+++ A.D./src/main/java/com/adoptdifferently/model/User.java
@@ -0,0 +1,9 @@
+public class user {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private String country;
+    private String city;
+    private String postcode;
+}
