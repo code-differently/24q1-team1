105dcc9418155fc01aebdc06344c18b24906637e
Moibrahi7 <160788202+Moibrahi7@users.noreply.github.com>
2024-04-24 14:54:44 -0400

    removed everything (#4)
    Co-authored-by: Mohamed <“moibrahi7@gmail.com”>

--- adoptionwebsite/src/main/java/com/24Q1-team1/model/user.java
+++
@@ -1,9 +0,0 @@
-public class user {
-    private String name;
-    private String email;
-    private String phoneNumber;
-    private String password;
-    private String country;
-    private String city;
-    private String postcode;
-}
