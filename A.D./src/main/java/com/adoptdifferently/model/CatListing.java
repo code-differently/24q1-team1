6fe23d62df040e4f5b6b55045d9b596e7c149aab
Moibrahi7 <160788202+Moibrahi7@users.noreply.github.com>
2024-04-24 15:14:14 -0400

    Feat: added base file structures (#6)
    Co-authored-by: Mohamed <“moibrahi7@gmail.com”>

---
+++ A.D./src/main/java/com/adoptdifferently/model/CatListing.java
@@ -0,0 +1,10 @@
+public class CatListing {
+
+    private long id;
+    private long ownerId;
+    private String name;
+    private String breed;
+    private int age;
+    private String location;
+    private String description;
+}
