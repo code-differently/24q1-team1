0e5e131346bf11e9a42739a509d9e6c26426cdf0
Mohamed <moibrahi7@gmail.com>
2024-04-24 16:28:27 -0400

    Feat: added Admin Controller base and setters and getters for the models

--- A.D./src/main/java/com/adoptdifferently/model/Admin.java
+++ A.D./src/main/java/com/adoptdifferently/model/Admin.java
@@ -1,5 +1,30 @@
 public class Admin {
 
   private String username;
-  private String password;  
+  private String password; 
+  private UUID adminId; 
+
+  public Admin(String username, String password, UUID adminId) {
+    this.username = username;
+    this.password = password;
+    this.adminId = adminId;
+  }
+  // Getters and setters
+
+  public UUID getAdminId() {
+    return adminId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public boolean isAdmin() {
+    return true;
+  }
+
 }
